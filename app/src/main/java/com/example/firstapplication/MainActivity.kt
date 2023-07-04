ff552748ed93974c80c2e751e6315f8b8e21c04c
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-07-03 21:24:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.firstapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstapplication.ui.theme.FirstApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val textView1 = findViewById<TextView>(R.id.idTVHeading1)
+
+        button1?.setOnClickListener() {
+            textView1.text = "you pressed the button!"
+        }
+    }
+}
+
